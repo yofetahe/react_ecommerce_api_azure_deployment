0dfcb3f8f8bd193b2f750103374db00d712080c3
Yofatehe <42419407+yofetahe@users.noreply.github.com>
2019-09-19 20:06:14 -0700

    first build commit

---
+++ build/precache-manifest.0b0875677835dcc0ab2092233d2e86ac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb75989ffdad28b1470283636449c7aa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4779ae96df3f79b15b50",
+    "url": "/static/css/main.d743e5dc.chunk.css"
+  },
+  {
+    "revision": "9226c5e091d1f46b22d5",
+    "url": "/static/js/2.4036ff66.chunk.js"
+  },
+  {
+    "revision": "4779ae96df3f79b15b50",
+    "url": "/static/js/main.814bc5f0.chunk.js"
+  },
+  {
+    "revision": "086038abfb8aea4737c9",
+    "url": "/static/js/runtime~main.8242c32e.js"
+  }
+]);
